85b9dad1a951632791a4113846fe8cfec9482ed0
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2026-04-20 14:32:10 +0700

    tes ci/cd

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+// TES CI/CD
 class Answer extends Model
 {
     use HasFactory;
